1d9a2a824048df504e0e15ada13b0f55a48a47b7
unknown <osamaelhenaweey@gmail.com>
2024-08-17 15:46:21 +0300

    Initial commit

--- main.js
+++ main.js
@@ -37,113 +37,87 @@ if (currentTheme) {
 // the number of tasks
 let counter;
 let numberIterationContainer = document.getElementById("numbers");
+let tasks = document.getElementById("todo__tasks");
+let collection = tasks.children;
+let activeElements = Array.from(collection).filter((element) =>
+    element.classList.contains("active")
+);
+// activeElements.map((e) => e.remove());
+console.log(activeElements);
 
-let tasks = document.querySelectorAll(".todo__tasks__task");
-
-let btnRemove = document.querySelectorAll(".remove_task");
-function removingTask() {
-    for (let task = 0; task < tasks.length; task++) {
-        /////////////////REMOVE/////////////////////////////////////
-        btnRemove[task].addEventListener("click", function () {
-            // /////when i click in the x button the father element will ber removed
-
-            // btnRemove[el].parentElement.classList.add("aaa");
-            btnRemove[task].parentElement.remove();
-        });
-    }
-}
-removingTask();
-let btnCheck = document.querySelectorAll(".check");
-function makeChecking() {
-    for (let task = 0; task < tasks.length; task++) {
-        /////////////////////ADDING styles to component/////////////////////////
-        btnCheck[task].addEventListener("click", function () {
-            // to add class i will do that when finish a task in todo list
-            tasks[task].classList.toggle("active");
-        });
-    }
-}
-makeChecking();
-// // check if you press complete button it will show only completed tasks and display none for all other tasks
-
-let btnComplete = document.getElementById("complete");
-
-for (let el = 0; el < tasks.length; el++) {
-    btnComplete.addEventListener("click", function () {
-        console.log(tasks[el]);
-
-        if (tasks[el].classList.value.includes("active")) {
-            tasks[el].classList.remove("non");
-        } else {
-            // console.log("bye");
-            tasks[el].classList.add("non");
-        }
-    });
-}
-// // check if you press active button it will show only active tasks and display none for all other tasks
-
-let btnActive = document.getElementById("active");
-
-for (let el = 0; el < tasks.length; el++) {
-    /////////////////////ADDING styles to component/////////////////////////
-    btnActive.addEventListener("click", function () {
-        counter = 0;
-        if (tasks[el].classList.value.includes("active")) {
-            tasks[el].classList.add("non");
-            //
-        } else {
-            // tasks[el].classList.add("non");
-
-            tasks[el].classList.remove("non");
-
-            //
-        }
-    });
-}
-
-let btnAll = document.getElementById("all");
-for (let el = 0; el < tasks.length; el++) {
-    /////////////////////ADDING styles to component/////////////////////////
-    btnAll.addEventListener("click", function () {
-        counter = 0;
-        if (tasks[el].classList.value.includes("active")) {
-            tasks[el].classList.remove("non");
-
-            //
-        } else {
-            // console.log("bye");
-            // tasks[el].classList.add("non");
-            console.log("hiiii");
-            tasks[el].classList.remove("non");
-
-            //
-        }
-    });
-}
 let btnClear = document.getElementById("clear");
 
-for (let el = 0; el < tasks.length; el++) {
-    /////////////////////ADDING styles to component/////////////////////////
-    btnClear.addEventListener("click", function () {
-        if (tasks[el].classList.value.includes("active")) {
-            tasks[el].remove();
-            // console.log(tasks[el]);
+btnClear.addEventListener("click", function () {
+    activeElements.map((e) => e.remove());
+});
 
-            //
-        }
-    });
-}
+// // check if you press complete button it will show only completed tasks and display none for all other tasks
 
-function makeIterations(tasks) {
-    counter = 0;
-    for (let el = 0; el < tasks.length; el++) {
-        if (!tasks[el].classList.value.includes("active")) {
-            counter++;
-            numberIterationContainer.innerHTML = `${counter} iterations left`;
-        }
-    }
-}
-makeIterations(tasks);
+// let btnComplete = document.getElementById("complete");
+
+// for (let el = 0; el < tasks.length; el++) {
+//     btnComplete.addEventListener("click", function () {
+//         console.log(tasks[el]);
+
+//         if (tasks[el].classList.value.includes("active")) {
+//             tasks[el].classList.remove("non");
+//         } else {
+//             // console.log("bye");
+//             tasks[el].classList.add("non");
+//         }
+//     });
+// }
+// // check if you press active button it will show only active tasks and display none for all other tasks
+
+// let btnActive = document.getElementById("active");
+
+// for (let el = 0; el < tasks.length; el++) {
+//     /////////////////////ADDING styles to component/////////////////////////
+//     btnActive.addEventListener("click", function () {
+//         counter = 0;
+//         if (tasks[el].classList.value.includes("active")) {
+//             tasks[el].classList.add("non");
+//             //
+//         } else {
+//             // tasks[el].classList.add("non");
+
+//             tasks[el].classList.remove("non");
+
+//             //
+//         }
+//     });
+// }
+
+// let btnAll = document.getElementById("all");
+// for (let el = 0; el < tasks.length; el++) {
+//     /////////////////////ADDING styles to component/////////////////////////
+//     btnAll.addEventListener("click", function () {
+//         counter = 0;
+//         if (tasks[el].classList.value.includes("active")) {
+//             tasks[el].classList.remove("non");
+
+//             //
+//         } else {
+//             // console.log("bye");
+//             // tasks[el].classList.add("non");
+//             console.log("hiiii");
+//             tasks[el].classList.remove("non");
+
+//             //
+//         }
+//     });
+// }
+
+// function makeIterations(tasks) {
+//     counter = 0;
+//     for (let el = 0; el < tasks.length; el++) {
+//         if (!tasks[el].classList.value.includes("active")) {
+//             counter++;
+//             numberIterationContainer.innerHTML = `${counter} iterations left`;
+//         }
+//     }
+// }
+// makeIterations(tasks);
 // numberIterationContainer.innerHTML = `${counter} iterations`;
 
 let task_container = document.getElementById("todo__tasks");
@@ -166,3 +140,64 @@ function addHtml() {
     });
 }
 addHtml();
+
+let todoContainer = document.getElementById("todo");
+
+todoContainer.addEventListener("click", function (e) {
+    // console.log(e.target.classList.value.includes("fa-check"));
+
+    // Remove
+    if (e.target.classList.value.includes("fa-xmark")) {
+        e.target.parentElement.parentElement.remove();
+    }
+    // Active
+    if (e.target.classList.value.includes("fa-check")) {
+        e.target.parentElement.parentElement.classList.toggle("active");
+    }
+    // btn complete
+    // for (let el = 0; el < tasks.length; el++) {
+    //     btnComplete.addEventListener("click", function () {
+    //         console.log(tasks[el]);
+
+    //         if (tasks[el].classList.value.includes("active")) {
+    //             tasks[el].classList.remove("non");
+    //         } else {
+    //             // console.log("bye");
+    //             tasks[el].classList.add("non");
+    //         }
+    //     });
+    // }
+    console.log(e.target);
+    // if (e.target.classList.value.includes("fa-check")) {
+    //     e.target.parentElement.parentElement.classList.toggle("active");
+    // }
+});
+//tasks[task].classList.toggle("active");
+
+// legacy
+// let tasks = document.querySelectorAll(".todo__tasks__task");
+
+// let btnRemove = document.querySelectorAll(".remove_task");
+// function removingTask() {
+//     for (let task = 0; task < tasks.length; task++) {
+//         /////////////////REMOVE/////////////////////////////////////
+//         btnRemove[task].addEventListener("click", function () {
+//             // /////when i click in the x button the father element will ber removed
+
+//             // btnRemove[el].parentElement.classList.add("aaa");
+//             btnRemove[task].parentElement.remove();
+//         });
+//     }
+// }
+// removingTask();
+// let btnCheck = document.querySelectorAll(".check");
+// function makeChecking() {
+//     for (let task = 0; task < tasks.length; task++) {
+//         /////////////////////ADDING styles to component/////////////////////////
+//         btnCheck[task].addEventListener("click", function () {
+//             // to add class i will do that when finish a task in todo list
+//             tasks[task].classList.toggle("active");
+//         });
+//     }
+// }
+// makeChecking();
